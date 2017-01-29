0f8f73bb13854aadd9285213179bee8909601dff
firefoxic <firefoxic.arts@gmail.com>
2017-01-29 15:24:48 +0300

    Замени формы при выборе изображения
    При изменении значения в поле загрузки изображения, то есть, как только
    пользовател выбрал файл фотографии, — показываем форму кадрирования и
    скрываем форму загрузки.

--- js/form.js
+++ js/form.js
@@ -1,1 +1,10 @@
 'use strict';
+
+var uploadOverlay = document.querySelector('.upload-overlay');
+var uploadSelectImage = document.querySelector('#upload-select-image');
+var uploadFile = uploadSelectImage.querySelector('#upload-file');
+
+uploadFile.addEventListener('change', function () {
+  uploadOverlay.classList.remove('invisible');
+  uploadSelectImage.classList.add('invisible');
+});
