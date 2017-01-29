3e06c902637e63db4af417b0180464525a20f267
firefoxic <firefoxic.arts@gmail.com>
2017-01-29 15:43:10 +0300

    Сделай обратную смену форм
    При нажатии на кнопку с крестиком форма кадрирования закрывается
    и покаызывается форма загрузки изображения.

--- js/form.js
+++ js/form.js
@@ -1,10 +1,16 @@
 'use strict';
 
 var uploadOverlay = document.querySelector('.upload-overlay');
+var uploadFormCansel = uploadOverlay.querySelector('.upload-form-cancel');
 var uploadSelectImage = document.querySelector('#upload-select-image');
 var uploadFile = uploadSelectImage.querySelector('#upload-file');
 
 uploadFile.addEventListener('change', function () {
   uploadOverlay.classList.remove('invisible');
   uploadSelectImage.classList.add('invisible');
+});
+
+uploadFormCansel.addEventListener('click', function () {
+  uploadOverlay.classList.add('invisible');
+  uploadSelectImage.classList.remove('invisible');
 });
