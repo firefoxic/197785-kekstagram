32fed4ab291622162f260ef59d6f99dfab40a7f4
firefoxic <firefoxic.arts@gmail.com>
2017-01-30 18:40:58 +0300

    Добавь применение фильтра к изображению
    При выборе фильтра он должен примениться к превью изображения.
    Также фильтр должен сниматься, если выбран не он. Код должен работать.
    Но по какой-то причине все выбранные фильтры не снимаются, хотя
    в примере http://codepen.io/Firefoxic/pen/rjpYNL с идентичным кодом
    классы не соответстующие выбранной радио-кнопке нормально снимаются.

--- js/form.js
+++ js/form.js
@@ -4,6 +4,8 @@ var uploadOverlay = document.querySelector('.upload-overlay');
 var uploadFormCansel = uploadOverlay.querySelector('.upload-form-cancel');
 var uploadSelectImage = document.querySelector('#upload-select-image');
 var uploadFile = uploadSelectImage.querySelector('#upload-file');
+var uploadFilter = document.querySelectorAll('[name="upload-filter"]');
+var filterImagePreview = document.querySelector('.filter-image-preview');
 
 uploadFile.addEventListener('change', function () {
   uploadOverlay.classList.remove('invisible');
@@ -14,3 +16,14 @@ uploadFormCansel.addEventListener('click', function () {
   uploadOverlay.classList.add('invisible');
   uploadSelectImage.classList.remove('invisible');
 });
+
+for (var i = 0; i < uploadFilter.length; i++) {
+  uploadFilter[i].addEventListener('change', function (event) {
+    var filterName = 'filter-' + event.currentTarget.value;
+    filterImagePreview.classList.add(filterName);
+  });
+  uploadFilter[i].addEventListener('blur', function (event) {
+    var filterName = 'filter-' + event.currentTarget.value;
+    filterImagePreview.classList.remove(filterName);
+  });
+}
