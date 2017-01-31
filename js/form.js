6b126595c3e5ed918f13cfc9c4f0303588e96d8a
firefoxic <firefoxic.arts@gmail.com>
2017-01-31 13:09:20 +0300

    Добавь изменение масштаба изображения
    При нажатии на кнопки - и + должно изменяться значение поля между ними.
    В соответствии с изменением значения должен изменяться масштаб изображения.
    Минимальное значение — 25%.
    Максимальное значение — 100%.
    Шагом изменения значения — 25%.
    Значение по умолчанию — 100%.

--- js/form.js
+++ js/form.js
@@ -6,9 +6,18 @@ var uploadSelectImage = document.querySelector('#upload-select-image');
 var uploadFile = uploadSelectImage.querySelector('#upload-file');
 var uploadFilter = document.querySelectorAll('[name="upload-filter"]');
 var filterImagePreview = document.querySelector('.filter-image-preview');
+var controlsValue = document.querySelector('.upload-resize-controls-value');
+var buttonDec = document.querySelector('.upload-resize-controls-button-dec');
+var buttonInc = document.querySelector('.upload-resize-controls-button-inc');
+var scaleMin = 0.25;
+var scaleMax = 1;
+var scaleStep = 0.25;
+var scaleDefault = 1;
+var scaleCurrent = scaleDefault;
 
 uploadFile.addEventListener('change', function () {
   removeFilter();
+  resizeImage();
   uploadOverlay.classList.remove('invisible');
   uploadSelectImage.classList.add('invisible');
 });
@@ -26,10 +35,31 @@ for (var i = 0; i < uploadFilter.length; i++) {
   });
 }
 
+buttonDec.addEventListener('click', function () {
+  var dec = +(scaleCurrent - scaleStep).toFixed(3);
+  if (scaleCurrent > scaleMin) {
+    scaleCurrent = (dec > scaleMin) ? dec : scaleMin;
+    resizeImage();
+  }
+});
+
+buttonInc.addEventListener('click', function () {
+  var inc = +(scaleCurrent + scaleStep).toFixed(3);
+  if (scaleCurrent < scaleMax) {
+    scaleCurrent = (inc < scaleMax) ? inc : scaleMax;
+    resizeImage();
+  }
+});
+
 function removeFilter() {
   for (var j = 0; j < filterImagePreview.classList.length; j++) {
     if (filterImagePreview.classList[j] !== 'filter-image-preview' && filterImagePreview.classList[j].substring(0, 7) === 'filter-') {
       filterImagePreview.classList.remove(filterImagePreview.classList[j]);
     }
   }
+}
+
+function resizeImage() {
+  controlsValue.value = +(scaleCurrent * 100).toFixed(5) + '%';
+  filterImagePreview.style.transform = 'scale(' + scaleCurrent + ')';
 }
