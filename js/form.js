13b8e16fda847f99be023bf451d5fc7679bdbfdc
firefoxic <firefoxic.arts@gmail.com>
2017-02-12 19:06:16 +0300

    Добавь доступности диалогу обработки фотографии
    Выбор фильтра и кнопка скрытия диалога должны быть доступны с клавиатуры
    по нажатию Enter или Space.
    
    Выбор с клавиатуры почему-то не вызывает события change, поэтому его
    проиходится генерировать для корректной работы применения фильтра
    к превью фотографии. Есть надежда, что это временное решение.
    
    Несмотря на то, что Space в Firefox почему-то не имеет keyCode, всё же
    лучше его тоже обрабатывать, пусть и по charCode. Потому что экранная
    читалка после нажатия на Enter не озвучивает «Выбрано», а после нажатия
    на Space — озвучивает (при смене `aria-checked`).
    
    Так же необходима обработка нажатия на Escape при открытом диалоге,
    которое должно закрывать этот диалог, если фокус не находится в поле
    написания комментария.

--- js/form.js
+++ js/form.js
@@ -1,48 +1,113 @@
 'use strict';
 
 var uploadOverlay = document.querySelector('.upload-overlay');
-var uploadFormCansel = uploadOverlay.querySelector('.upload-form-cancel');
+var uploadCansel = uploadOverlay.querySelector('#upload-cancel');
 var uploadSelectImage = document.querySelector('#upload-select-image');
 var uploadFile = uploadSelectImage.querySelector('#upload-file');
 var uploadFilterControls = document.querySelector('.upload-filter-controls');
 var filterImagePreview = document.querySelector('.filter-image-preview');
-var controlsValue = document.querySelector('.upload-resize-controls-value');
-var buttonDec = document.querySelector('.upload-resize-controls-button-dec');
-var buttonInc = document.querySelector('.upload-resize-controls-button-inc');
+var resizeControls = uploadOverlay.querySelector('.upload-resize-controls');
+var resizeControlsValue = resizeControls.querySelector('.upload-resize-controls-value');
+var resizeControlsDec = resizeControls.querySelector('.upload-resize-controls-button-dec');
+var resizeControlsInc = resizeControls.querySelector('.upload-resize-controls-button-inc');
 var scaleMin = 0.25;
 var scaleMax = 1;
 var scaleStep = 0.25;
 var scaleDefault = 1;
 var scaleCurrent = scaleDefault;
 
+var ENTER_KEY_CODE = 13;
+var SPACE_CHAR_CODE = 32;
+var ESCAPE_KEY_CODE = 27;
+
+var isActivateEvent = function (event) {
+  var enter = event.keyCode === ENTER_KEY_CODE;
+  var space = event.charCode === SPACE_CHAR_CODE;
+  return (event.keyCode || event.charCode) && (enter || space);
+};
+
+var toggleAttribute = function (attribute, element) {
+  var newValue = !(element.getAttribute(attribute) === 'true');
+  element.setAttribute(attribute, newValue);
+};
+
+var toggleForms = function () {
+  uploadOverlay.classList.toggle('invisible');
+  uploadSelectImage.classList.toggle('invisible');
+  toggleAttribute('aria-hidden', uploadOverlay);
+  toggleAttribute('aria-hidden', uploadSelectImage);
+};
+
+var uploadFormDescription = document.querySelector('.upload-form-description');
+var setEscapeHandler = function (event) {
+  if ((event.target !== uploadFormDescription) && (event.keyCode === ESCAPE_KEY_CODE)) {
+    event.preventDefault();
+    toggleForms();
+  }
+};
+
 var resizeImage = function () {
-  controlsValue.value = +(scaleCurrent * 100).toFixed(5) + '%';
+  resizeControlsValue.value = +(scaleCurrent * 100).toFixed(5) + '%';
   filterImagePreview.style.transform = 'scale(' + scaleCurrent + ')';
 };
 
-var removeFilter = function () {
+var removeFilterFromPreview = function () {
   filterImagePreview.className = filterImagePreview.classList[0];
 };
 
+var generateEvent = function (type, element) {
+  var newEvent = document.createEvent('Event');
+  newEvent.initEvent(type, true, true);
+  element.dispatchEvent(newEvent);
+};
+
 uploadFile.addEventListener('change', function () {
-  removeFilter();
+  removeFilterFromPreview();
   resizeImage();
-  uploadOverlay.classList.remove('invisible');
-  uploadSelectImage.classList.add('invisible');
+  toggleForms();
+  document.addEventListener('keypress', setEscapeHandler);
 });
 
-uploadFormCansel.addEventListener('click', function () {
-  uploadOverlay.classList.add('invisible');
-  uploadSelectImage.classList.remove('invisible');
+uploadCansel.addEventListener('click', function () {
+  toggleForms();
+  document.removeEventListener('keypress', setEscapeHandler);
+});
+
+uploadCansel.addEventListener('keypress', function (event) {
+  if (isActivateEvent(event)) {
+    event.preventDefault();
+    document.removeEventListener('keypress', setEscapeHandler);
+    toggleForms();
+  }
+});
+
+uploadFilterControls.addEventListener('keypress', function (event) {
+  if (isActivateEvent(event)) {
+    event.preventDefault();
+    var filterId = event.target.getAttribute('for');
+    var filterSelectedButton = uploadFilterControls.querySelector('#' + filterId);
+    filterSelectedButton.checked = true;
+    generateEvent('change', filterSelectedButton);
+  }
 });
 
 uploadFilterControls.addEventListener('change', function (event) {
-  removeFilter();
-  var filterName = 'filter-' + event.target.value;
-  filterImagePreview.classList.add(filterName);
+  var filterName = event.target.value;
+  var filter = 'filter-' + filterName;
+  var filterId = event.target.getAttribute('id');
+  var filterLabel = document.querySelector('[for="' + filterId + '"]');
+  var filterLabels = event.target.parentNode.querySelectorAll('label');
+  filterLabel.setAttribute('aria-checked', true);
+  filterLabels.forEach(function (item) {
+    if (item !== filterLabel) {
+      item.setAttribute('aria-checked', false);
+    }
+  });
+  removeFilterFromPreview();
+  filterImagePreview.classList.add(filter);
 });
 
-buttonDec.addEventListener('click', function () {
+resizeControlsDec.addEventListener('click', function () {
   var dec = +(scaleCurrent - scaleStep).toFixed(3);
   if (scaleCurrent > scaleMin) {
     scaleCurrent = (dec > scaleMin) ? dec : scaleMin;
@@ -50,7 +115,7 @@ buttonDec.addEventListener('click', function () {
   }
 });
 
-buttonInc.addEventListener('click', function () {
+resizeControlsInc.addEventListener('click', function () {
   var inc = +(scaleCurrent + scaleStep).toFixed(3);
   if (scaleCurrent < scaleMax) {
     scaleCurrent = (inc < scaleMax) ? inc : scaleMax;
